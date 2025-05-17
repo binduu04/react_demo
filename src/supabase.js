848b197dfc64c187710467049458945d9aed4e05
Bindushree B <bindushree1978@gmail.com>
2025-05-17 13:37:31 +0530

    commit to update env

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://jorizaqqovaimqwecxgb.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impvcml6YXFxb3ZhaW1xd2VjeGdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0MDUwNTQsImV4cCI6MjA2MTk4MTA1NH0.E8b2dJiWk5mM9m1tfuCbQtP_vtwHoBXpLvSqAOX87GE"
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey)
 export default supabase;
 // import { createClient } from '@supabase/supabase-js'
